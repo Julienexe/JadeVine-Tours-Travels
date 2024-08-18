9c72a02cb21dacb826145888b4cacd3f1ddffc03
Julianexe <julianssonko@gmail.com>
2024-08-18 16:18:08 +0300

    modify models

--- jv/models.py
+++ jv/models.py
@@ -16,4 +16,7 @@ class Place(models.Model):
     imageUrl = models.CharField(max_length=250)
     category = models.ForeignKey(Category,on_delete=models.CASCADE, related_name="places")
     description = models.TextField()
-    price = models.IntegerField()
+    price = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
