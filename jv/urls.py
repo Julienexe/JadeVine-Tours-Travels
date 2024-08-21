705028041b97ad8537a4389c69cd261f71d5d14b
Michael Rusezera <michaelrusezera@gmail.com>
2024-08-21 21:40:28 +0300

    changes

--- jv/urls.py
+++ jv/urls.py
@@ -6,4 +6,9 @@ app_name = 'jv'
 urlpatterns = [
     # Add your URL patterns here
     path('', views.home, name='home'),
+    path("popular_places/",views.popular_places,name='popular_places'),
+    path("services/",views.services,name='services'),
+    path("contact/",views.contact,name='contact'),
+    path("about/",views.about,name='about'),
+    path("gallery/",views.gallery,name='gallery'),
 ]
--- jv/views.py
+++ jv/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .models import *
 
 def home(request):
     return render(request,"jv/index.html")
+def gallery(request):
+    return render(request,"jv/gallery.htm")
+def about(request):
+    return render(request,"jv/about.htm")
+def contact(request):
+    return render(request,"jv/contact.htm")
+def services(request):
+    return render(request,"jv/services.htm")
+def popular_places(request):
+    places = Place.objects.all()
+    print(places)
+    context = {'places':places}
+    return render(request,"jv/popular_places.htm",context)
 
