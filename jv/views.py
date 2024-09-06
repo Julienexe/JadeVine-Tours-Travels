f82de7ba46aadce6f65f6fd84bd006079a7e8a48
Julianexe <julianssonko@gmail.com>
2024-09-06 15:34:41 +0300

    client changes

--- jv/views.py
+++ jv/views.py
@@ -114,12 +114,7 @@ def create_inquiry(request):
 
 @csrf_exempt
 def contact_us(request):
-    #to be populated later
-    response_data = {
-                'status': 1,
-                'message': 'Message submitted successfully!',
-            }
-    return JsonResponse(response_data)
+    return render(request,"jv/contact.htm")
     
 
 def place(request,place_id):
