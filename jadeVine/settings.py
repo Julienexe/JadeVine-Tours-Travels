7919162eea5a5fcd4a25e1563f714c1996819958
Julianexe <julianssonko@gmail.com>
2024-09-06 15:35:54 +0300

    change to test db

--- jadeVine/settings.py
+++ jadeVine/settings.py
@@ -89,12 +89,12 @@ WSGI_APPLICATION = 'jadeVine.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default=os.environ.get("DATABASE_URL"),
+        default=os.environ.get("DB_URL"), 
         conn_max_age=600
     )
 }
 
-# Password validation
+# Password validation 
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
